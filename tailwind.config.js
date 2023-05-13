23a29c31e51db0fc31d873c6fc7be378049442a9
John B <johnbernalfsd@gmail.com>
2023-05-13 03:31:24 -0600

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./plugin-src/**/*.{js,ts,jsx,tsx}",
+    "./ui-src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
